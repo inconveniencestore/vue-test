88c6efb62f3966fe67a45269f500461edfcdff3f
inconveniencestore <iivyleaves@gmail.com>
2025-01-14 12:30:50 -0500

    afd as fath bert nrt

---
+++ vue/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="aaaa">
+    <h1>{{ count }}</h1>
+    <button @click="increment">Click Me</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+let count = ref(0)
+
+function increment() {
+  count.value++
+}
+</script>
+
+<style scoped></style>
