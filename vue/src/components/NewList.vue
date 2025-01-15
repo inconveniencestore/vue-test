24c1734df4ad138b7f033cf882c77f2c1e57b04d
inconveniencestore <iivyleaves@gmail.com>
2025-01-15 12:30:45 -0500

    what in the WORLD is going on

---
+++ vue/src/components/NewList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2 v-for="animal in animals" :key="animal">{{ animal }}</h2>
+    <!-- its like a for each -->
+  </div>
+</template>
+
+<script setup>
+const animals = ['zebra', 'panda', 'cat', 'whale']
+console.log('jfhakdfksdjh')
+</script>
+
+<style scoped></style>
+
+<!--vbase-3-setup-->
---
+++ vue/src/views/ImportView.vue
@@ -0,0 +1,18 @@
+<template>
+  <form>
+    <div class="username">
+      <label for="">username: </label>
+      <input type="text" />
+    </div>
+    <div class="password">
+      <label for="">password: </label>
+      <input type="text" />
+    </div>
+  </form>
+</template>
+
+<script setup>
+import ReactivityForm from '@/components/ReactivityForm.vue'
+</script>
+
+<style lang="scss" scoped></style>
