7306ca9a7c44052733e435a96cd787b76aef7f07
inconveniencestore <iivyleaves@gmail.com>
2025-01-16 12:28:54 -0500

    ok we better be done

--- vue/src/components/ReactivityForm.vue
+++ vue/src/components/ReactivityForm.vue
@@ -1,7 +1,34 @@
-<template></template>
+<template>
+  <form name="login-form" @submit.prevent="login(forom)">
+    <div class="username">
+      <label for="username">username: </label>
+      <input type="text" v-model="forom.username" />
+    </div>
+    <div class="password">
+      <label for="password">password: </label>
+      <input type="password" v-model="forom.password" />
+    </div>
+    <button @submit="console.log(forom.username, forom.password)">submit</button>
+  </form>
+  <h2 v-if="cat">A</h2>
+  <h2 v-else>B</h2>
+</template>
 
-<script setup></script>
+<script setup>
+import { reactive } from 'vue'
+
+const forom = reactive({
+  username: '',
+  password: '',
+})
+function login(forom) {
+  console.log(`${forom.username}, ${forom.password}`)
+}
+reactive
+const cat = false
+</script>
 
 <style scoped></style>
 
 <!--vbase-3-setup-->
+<!--we use composition api-->
--- vue/src/views/ImportView.vue
+++ vue/src/views/ImportView.vue
@@ -1,18 +1,11 @@
 <template>
-  <form>
-    <div class="username">
-      <label for="">username: </label>
-      <input type="text" />
-    </div>
-    <div class="password">
-      <label for="">password: </label>
-      <input type="text" />
-    </div>
-  </form>
+  <div>
+    <ReactivityForm></ReactivityForm>
+  </div>
 </template>
 
 <script setup>
 import ReactivityForm from '@/components/ReactivityForm.vue'
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped></style>
